cb4dc87df67c98881883a0f93d71461ce0ca9bea
diego.lipa <diego.lipa@lim.upeu.edu.pe>
2024-05-25 22:32:26 -0500

    coneccion mysql

---
+++ src/main/java/com/dlp/api_java_mysql/ApiJavaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.dlp.api_java_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlp/api_java_mysql/ApiJavaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlp.api_java_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
